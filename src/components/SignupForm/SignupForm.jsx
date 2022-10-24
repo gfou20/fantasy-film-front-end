3f0409e95a35ca99b41831ad8e1205bcc526664f
GFou <gfouchard20@outlook.com>
2022-10-24 15:59:03 -0400

    change sign up navigation

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -30,7 +30,7 @@ const SignupForm = props => {
     try {
       await authService.signup(formData, photoData.photo)
       props.handleSignupOrLogin()
-      navigate('/movie-list')
+      navigate('/movie-search')
     } catch (err) {
       props.updateMessage(err.message)
     }
